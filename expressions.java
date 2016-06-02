6083065af75fc8fa85e310d30251ceeea2d2f3e1
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-06-02 10:58:45 -0500

    Expressions expressed
    Homework completed

--- expressions.java
+++ expressions.java
@@ -1,20 +1,58 @@
+public class expressions {
+    public static void main(String[] args) {
+
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n"); // I like a clean screen
+
 /**
  *  Expressions Homework
  */
- 
+
 // Part 1: Order Of Operations
+
+/*
+ *  Please Excuse My Demented Aunt Stuart
+ *  Parentheses, Exponentiation, Multiplication, Division, Addition, Subtraction
+ */
+        System.out.println("Part 1: Order Of Operations");
+        System.out.println("/*\n * Please Excuse My Demented Aunt Stuart");
+        System.out.println(" * Parentheses, Exponentiation, Multiplication, Division, Addition, Subtraction");
+        System.out.println(" */");
+        System.out.println("-------------------------------------------------------------------------------");
+
 // ---------------------------
 
+// Part 2: Operators Warm-Up
+        System.out.println("Part 2: Operators Warm-Up");
+        System.out.printf("1) 42 + 1 = %d %n", 42 + 1);
+        System.out.printf("2) 0 * 981 + 68 * 8 = %d %n", 0 * 981 + 68 * 8);
+        System.out.printf("3) 3.14 + 0.09 - 12 = %.2f %n", 3.14 + 0.09 - 12);
+        System.out.printf("4) 89 %% 4 = %d %n", 89 % 4);
+        System.out.printf("5) (66.7128 - 5.16) / 5 = %.5f", (66.7128 - 5.16) / 5);
+        System.out.println("\n-------------------------------------------------------------------------------");
 
-// Part 2: Operators
 // ---------------------------
 
+// Part 3: Geometry
 
+        System.out.println("Part 3: Geometry");
+        System.out.println("1) A = Math.PI * Math.pow(r, 2);");
+        System.out.println("2) Math.pow(a, 2) + Math.pow(b, 2) = Math.pow(c, 2);");
+        System.out.println("3) A = b * h / 2;");
+        System.out.println("4) V = Math.pow(h, 3);");
+        System.out.printf("5) ms = 14 * 60 * 1000; (answer: %d)", 14 * 60 * 1000);
+        System.out.println("\n-------------------------------------------------------------------------------");
 
-// Part 3: Formulae
 // ---------------------------
 
+// Part 4: Brain Twisters (Bonus Challenge)
 
+        System.out.println("Part 4: Brain Twisters (Bonus Challenge)");
+        System.out.println("1) if( a == b / 3 ){do something}; (so long as a and b are same numeric type)");
+        System.out.println("2) int y = 2x + 1;  int x = (y - 1) / 2;");
+        System.out.println("3) m = (y2 - y1) / (x2 - x1);");
+        System.out.println("\n-------------------------------------------------------------------------------");
 
-// Part 4: Bonus
 // ---------------------------
+
+    }
+}
