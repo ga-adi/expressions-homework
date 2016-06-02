bfe926ae2dcefdf0c8c22e611283447831160a7f
ericaschulz <ericalynnschulz@gmail.com>
2016-06-02 11:00:14 -0500

    Homework submission

--- expressions.java
+++ expressions.java
@@ -1,20 +1,65 @@
 /**
  *  Expressions Homework
  */
- 
-// Part 1: Order Of Operations
-// ---------------------------
+
+/*
 
 
-// Part 2: Operators
-// ---------------------------
+Part 1: Operators
+Common operators include addition (+), subtraction (-), multiplication (*), division (/),
+and remainder (%)
+Part 2: Order of Operations
+Multiplication and division operators take precedence over addition and subtraction operators
+though an addition/subtraction equation can be given precedence with parentheses:
+For example, 2 + 8 * 6 = 50
+But (2 + 8) * 6 = 60
+And 6 + 6 / 2 = 9
+But (6 + 6) / 2 = 6
+
+*/
+
+public class Main {
+   public static void Main(String[] args) {
+
+int result = 42 + 1;
+System.out.println("42 + 1 = ") + result;
+int result = 0 * 981 + 68 * 8;
+System.out.println("0 * 981 then add 68 * 8 = ") + result;
+double result = 3.14 + .09 - 12;
+System.out.println("3.14 + .09 - 12 = ") + result;
+int result = 89 % 4;
+System.out.println("89 % 4 = ") + result;
+float result = (66.7128 - 5.16) / 5;
+System.out.println("(66.7128 - 5.16) / 5 = ") + result;
+
+
+    }
+}
+
+/*
+
+//Part 3: Formulae
+
+float circleArea = 3.14 * (r * );
+int hypotenuse = (a * a) + (b * b);
+int triangleArea = (h * b) / 2;
+int cubeVolume = a * a * a;
 
 
 
-// Part 3: Formulae
-// ---------------------------
+//Part 4: Bonus
+
+n = x / 2;
+(n + 2) = (x - 4);
+int slopeM = (y1 - y2) / (x1 - x2);
+
+*/
 
 
 
-// Part 4: Bonus
-// ---------------------------
+
+
+
+
+
+
